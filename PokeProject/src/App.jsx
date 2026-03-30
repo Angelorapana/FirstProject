3fcb5bf346165720d279e8d819a6f3a10c7a541e
Angelo Rapanà <angelorapana@icloud.com>
2026-03-30 17:15:19 +0200

    Fix router and pages

--- PokeProject/src/App.jsx
+++ PokeProject/src/App.jsx
@@ -1,14 +1,37 @@
-import { useState } from 'react'
-
+import { BrowserRouter, Routes, Route, Link } from 'react-router-dom'
+import Card from './Components/PokemonCards.jsx'
 
 function App() {
 
   return (
-    <>
-    <div className="container">
-      <h1 className="title">PokeProject</h1>
-    </div>
-    </>
+    
+      <BrowserRouter>
+
+        <nav className="navbar navbar-expand-lg bg-body-tertiary">
+          <Link className="navbar-brand" to="/">PokeProject</Link>
+
+          <div className="collapse navbar-collapse">
+            <ul className="navbar-nav">
+              <li className="nav-item">
+                <Link className="nav-link" to="/">Info</Link>
+              </li>
+              <li className="nav-item">
+                <Link className="nav-link" to="/card">Card</Link>
+              </li>
+              <li className="nav-item">
+                <Link className="nav-link" to="/contact">Contatti</Link>
+              </li>
+            </ul>
+          </div>
+        </nav>
+
+        <Routes>
+          <Route path='/' element={<h1>Info</h1>} />
+          <Route path='/card' element={<Card />} />
+          <Route path='/contact' element={<h1>Contatti</h1>} />
+        </Routes>
+
+      </BrowserRouter>
   )
 }
 
